0418000948a431d3ed107a94ade383e2af29d55b
Scott Carleton <scott@artsicle.com>
2013-05-09 15:48:44 -0400

    workaround xmlrpc for ruby 2.0

--- lib/hominid/api.rb
+++ lib/hominid/api.rb
@@ -4,11 +4,11 @@ module Hominid
     instance_methods.each do |m|
       undef_method m unless m.to_s =~ /^__|object_id|method_missing|respond_to?|to_s|inspect|kind_of?|nil?|should|should_not/
     end
-    
+
     include Hominid::Campaign
     include Hominid::List
     include Hominid::Security
-    
+
     # MailChimp API Documentation: http://apidocs.mailchimp.com/api/1.3/
     MAILCHIMP_API_VERSION = "1.3"
 
@@ -26,6 +26,7 @@ module Hominid
       protocol = @config[:secure] ? 'https' : 'http'
       @api_key = api_key
       @chimpApi = XMLRPC::Client.new2("#{protocol}://#{dc}.#{@config[:domain]}/#{@config[:api_version]}/", nil, @config[:timeout])
+      @chimpApi.http_header_extra = {'accept-encoding' => 'identity'}
     end
 
     def method_missing(api_method, *args) # :nodoc:
@@ -34,20 +35,20 @@ module Hominid
       super if error.faultCode == -32601
       raise APIError.new(error)
     end
-    
+
     def respond_to?(api_method) # :nodoc:
       @chimpApi.call(api_method, @api_key)
     rescue XMLRPC::FaultException => error
-      error.faultCode == -32601 ? false : true 
+      error.faultCode == -32601 ? false : true
     end
-    
+
     private
 
     def camelize_api_method_name(str)
       str.to_s[0].chr.downcase + str.gsub(/(?:^|_)(.)/) { $1.upcase }[1..str.size]
     end
   end
-  
+
   class APIError < StandardError
     attr_accessor :fault_code
     def initialize(error)
